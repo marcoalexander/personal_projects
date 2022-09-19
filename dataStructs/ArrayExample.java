a885376ec46bb4a3cf29a8842729b84652ec744a
marcoalexander <timmythetom132@gmail.com>
2022-09-19 13:46:34 -0500

    trying to push pc projects over to git that now has mac projects

---
+++ dataStructs/ArrayExample.java
@@ -0,0 +1,24 @@
+package com.codewithmosh;
+
+import java.util.Arrays;
+public class ArrayExample{
+	/*Arrays:
+		look up o(1)
+		insert  o(n)
+		delete	o(n)
+
+	*/
+
+	public static void main(String[] args) {
+		int[] numbers = new int[3];
+		System.out.println(numbers);
+		System.out.println(Arrays.toString(numbers));
+
+		int[] newNumbers = {10,20,30};
+		System.out.println(Arrays.toString(newNumbers));
+		System.out.println(newNumbers.length);
+
+		//building an array
+		
+	}
+}
